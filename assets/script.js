83c9bb018e74ad8c29e14c05265671d0eff491fd
AJoelj84 <ajoelj84@gmail.com>
2023-02-19 18:37:07 -0500

    added questions array

--- assets/script.js
+++ assets/script.js
@@ -9,25 +9,25 @@ var questions = [
         correctAnswer: 'Alerts'
     },
 
-    {question:'',
-        answers:[''],
-        correctAnswer:''
+    {question:'A very useful tool during development and debugging for printing content to the debugger is?',
+        answers:['JavaScript','Terminal/Bash','For Loops','Console Log'],
+        correctAnswer:'Console Log'
     },
 
-    {question:'',
-    answers:[''],
-    correctAnswer:''
+    {question:'Arrays in JavaScript can be used to store?',
+    answers:['Numbers and Strings','Other Arrays', 'Booleans','All of the Above'],
+    correctAnswer:'All of the Above'
     },
 
 
-    {question:'',
-    answers:[''],
-    correctAnswer:''
+    {question:'The condition in an if/else statement is enclosed with ____.',
+    answers:['Quotes','Curly Brackets','Parenthesis','Square Brackets'],
+    correctAnswer:'Parenthesis'
     },
 
-    {question:'',
-    answers:[''],
-    correctAnswer:''
+    {question:'String values must be enclosed within ____ when being assigned to variables.',
+    answers:['Commas','Curly Brackets','Quotes','Parenthesis'],
+    correctAnswer:'Quotes'
     },
 
 
