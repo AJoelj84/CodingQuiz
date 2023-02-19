abbc1d000d92dacd2b554adaa9297d5d583fc8dd
AJoelj84 <ajoelj84@gmail.com>
2023-02-19 11:56:00 -0500

    Setting up JS Variables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
+var startButton = document.getElementById('start-btn');
+var questionBox = document.getElementById('question-box');
+var answerButtonBox = document.getElementById('answer-button-box');
+var quizBox = document.getElementById('quiz-box');
+
+
 var answer1 = document.querySelector('');
 var answer2 = document.querySelector('');
 var answer3 = document.querySelector('');
