1c8b7598291005a0a2e03a47472f820c9866908d
AJoelj84 <ajoelj84@gmail.com>
2023-02-19 12:13:56 -0500

    More additions to functions of javascript

--- assets/script.js
+++ assets/script.js
@@ -12,4 +12,35 @@ var questions = [ { question: 'Commonly used data types do not include?',
 
 
 
-startButton.addEventListener('click', startQuiz);              
+startButton.addEventListener('click', startQuiz);  
+
+function startQuiz() {
+    startButton.classList.add('hide');
+    quizContainer.classList.remove('hide');
+    setNextQuestion();
+  }
+  
+  function setNextQuestion() {
+    resetState();
+    showQuestion(questions[currentQuestionIndex]);
+  }
+
+  function showQuestion(question) {
+    questionContainer.innerText = question.question;
+    question.answers.forEach(answer => {
+      var button = document.createElement('button');
+      button.innerText = answer.text;
+      button.classList.add('answer-btn');
+      if (answer.correct) {
+        button.dataset.correct = answer.correct;
+      }
+      button.addEventListener('click', selectAnswer);
+      answerButtonsContainer.appendChild(button);
+    });
+  }
+
+  
+
+
+
+
